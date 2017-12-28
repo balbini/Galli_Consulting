20392707075db242e6408e80a5db6a4214a0600c
balbini <balbini91@gmail.com>
2017-12-27 21:36:11 -0800

    built out routes, user registration working through postman

--- app.js
+++ app.js
@@ -4,11 +4,42 @@ const bodyParser = require('body-parser');
 const cors = require ('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to database
+mongoose.connect(config.database);
+
+// On connection
+mongoose.connection.on('connected', () => {
+  console.log("connected to database" +config.database)
+})
+// On error
+mongoose.connection.on('error', () => {
+  console.log("error to database" +config.database)
+})
 
 const app = express();
 
+const users = require('./routes/users');
+
+//Port Number
 const port = 3000;
+
+//Cors Middleware
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+  res.send("Invalid EndPoint sucka")
+});
 
 app.listen(port, () => {
   console.log('server started on port '+port);
-})
+});
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: 'mongodb://localhost:27017/galli_consulting_final',
+  secret: 'yoursecret'
+}

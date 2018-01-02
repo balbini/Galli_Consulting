c904a4716eca07b3368f375596a632f31d75449a
balbini <balbini91@gmail.com>
2018-01-02 15:38:36 -0800

    register user working

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -13,7 +15,11 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService: ValidateService, private flashMessage:FlashMessagesService) { }
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage:FlashMessagesService,
+    private authService:AuthService,
+    private router:Router) { }
 
   ngOnInit() {
   }
@@ -36,6 +42,17 @@ export class RegisterComponent implements OnInit {
     this.flashMessage.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
     return false;
   }
+
+  // Register User
+  this.authService.registerUser(user).subscribe(data => {
+    if(data.success){
+      this.flashMessage.show('You are now registered', {cssClass: 'alert-success', timeout: 3000});
+      this.router.navigate(['/login']);
+    } else {
+      this.flashMessage.show('Something is wrong', {cssClass: 'alert-danger', timeout: 3000});
+      this.router.navigate(['/register']);
+    }
+  });
   }
 
 }
